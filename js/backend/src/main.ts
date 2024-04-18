a8ade784d11dccb0d2fc19ac497f2aedbc1c0163
Nikita Karpov <jacquesjaws@yandex.ru>
2024-04-18 19:50:30 +0400

    test commit2

---
+++ js/backend/src/main.ts
@@ -0,0 +1,14 @@
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { JwtAuthGuard } from "./auth/jwt-auth.guard";
+import { ValidationPipe } from "@nestjs/common";
+
+async function start() {
+  const PORT = process.env.PORT || 5000;
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
+  await app.listen(PORT, () => console.log(`Сервер успешно работает`));
+}
+
+start();
